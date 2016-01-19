444d44d6eaec30183b6aba9e761d0d703d2a3a3a
Christian Glindkamp <christian.glindkamp@gmail.com>
2016-01-19 22:51:43 +0100

    listview: change the meaning of index
    The index member now denotes the selected item in the whole list instead
    of an offset to first. This greatly reduces the complexity in the model
    change handler.

--- src/listview.c
+++ src/listview.c
@@ -18,7 +18,7 @@ static void print_list(struct listview *view)
 		if(i + view->first >= listmodel_count(view->model))
 			break;
 
-		if(i == view->index) {
+		if(i + view->first == view->index) {
 			wattron(view->window, A_BOLD | COLOR_PAIR(1) );
 		} else {
 			wattroff(view->window, A_BOLD | COLOR_PAIR(1) );
@@ -32,19 +32,19 @@ static void print_list(struct listview *view)
 
 void listview_up(struct listview *view)
 {
-	if(view->index > 0)
+	if(view->index > 0) {
 		view->index--;
-	else if(view->first > 0)
-		view->first--;
+		if(view->first > view->index)
+			view->first = view->index;
+	}
 	print_list(view);
 }
 
 void listview_down(struct listview *view)
 {
-	if(view->first + view->index < listmodel_count(view->model) - 1) {
-		if(view->index < getmaxy(view->window) - 1)
-			view->index++;
-		else
+	if(view->index < listmodel_count(view->model) - 1) {
+		view->index++;
+		if(view->index - view->first > getmaxy(view->window) - 1)
 			view->first++;
 	}
 	print_list(view);
@@ -54,14 +54,13 @@ void listview_pageup(struct listview *view)
 {
 	int rowcount = getmaxy(view->window);
 
-	if(view->index > 0)
-		view->index = 0;
-	else {
+	if(view->index == view->first) {
 		if(view->first >= rowcount)
 			view->first -= rowcount;
 		else
 			view->first = 0;
 	}
+	view->index = view->first;
 	print_list(view);
 }
 
@@ -70,42 +69,39 @@ void listview_pagedown(struct listview *view)
 	int listcount = listmodel_count(view->model);
 	int rowcount = getmaxy(view->window);
 
-	if(view->index < rowcount - 1) {
+	if(view->index - view->first < rowcount - 1) {
 		if(listcount > rowcount)
-			view->index = rowcount - 1;
+			view->index = view->first + rowcount - 1;
 		else
 			view->index = listcount - 1;
 	} else {
-		if(view->first + view->index + rowcount < listcount)
-			view->first += rowcount;
+		if(view->index + rowcount < listcount)
+			view->index += rowcount;
 		else {
-			view->first = listcount - rowcount;
+			view->index = listcount - 1;
 		}
+		view->first = view->index - rowcount + 1;
 	}
 	print_list(view);
 }
 
 unsigned int listview_getindex(struct listview *view)
 {
-	return view->first + view->index;
+	return view->index;
 }
 
 void listview_resize(struct listview *view, unsigned int width, unsigned int height)
 {
-	unsigned int selected = view->first + view->index;
 	unsigned int count = listmodel_count(view->model);
 
 	wresize(view->window, height, width);
-	if(view->index >= height) {
-		view->index = height - 1;
-		view->first = selected - view->index;
-	}
+	if(view->index - view->first >= height)
+		view->first = view->index - height + 1;
 	if(view->first + height > count) {
 		if(height > count)
 			view->first = 0;
 		else
 			view->first = count - height;
-		view->index = selected - view->first;
 	}
 	print_list(view);
 }
@@ -114,38 +110,26 @@ static void change_cb(unsigned int index, enum model_change change, void *data)
 {
 	struct listview *view = data;
 	int rowcount = getmaxy(view->window);
+	int listcount = listmodel_count(view->model);
 
 	switch(change) {
 	case MODEL_ADD:
-		if(index < view->first)
-			view->first++;
-		else if(index <= view->first + view->index) {
-			if(listmodel_count(view->model) < rowcount)
-				view->index++;
-			else
+		if(index <= view->index) {
+			view->index++;
+			if(listcount > rowcount)
 				view->first++;
 			print_list(view);
 		} else if(index < view->first + rowcount)
 			print_list(view);
 		break;
 	case MODEL_REMOVE:
-		if(index < view->first)
-			view->first--;
-		else if(index <= view->first + view->index) {
-			if(view->first != 0) {
+		if(index <= view->index) {
+			view->index--;
+			if(view->first != 0)
 				view->first--;
-			} else
-				view->index--;
 			print_list(view);
-		} else if(index < view->first + rowcount) {
-			if(listmodel_count(view->model) < view->first + rowcount) {
-				if(view->first != 0) {
-					view->first--;
-					view->index++;
-				}
-			}
+		} else if(index < view->first + rowcount)
 			print_list(view);
-		}
 	case MODEL_CHANGE:
 		if(index >= view->first && index < view->first + rowcount)
 			print_list(view);
