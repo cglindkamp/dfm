417fe15624f5ef9fe70e356f0a54852c354d5d43
Christian Glindkamp <christian.glindkamp@gmail.com>
2017-07-17 10:57:02 +0200

    Mark all functions that can fail due to OOM with warn_unused_result

--- src/list.h
+++ src/list.h
@@ -8,12 +8,12 @@
 typedef struct list list_t;
 typedef void (*list_item_deallocator)(void *);
 
-list_t *list_new(size_t initial_size);
+list_t *list_new(size_t initial_size) __attribute__((warn_unused_result));
 void list_free(const list_t *list, list_item_deallocator deallocator);
 
 size_t list_length(const list_t *list);
-bool list_append(list_t *list, void *item);
-bool list_insert(list_t *list, size_t index, void *item);
+bool list_append(list_t *list, void *item) __attribute__((warn_unused_result));
+bool list_insert(list_t *list, size_t index, void *item) __attribute__((warn_unused_result));
 void list_remove(list_t *list, size_t index);
 void *list_get_item(const list_t *list, size_t index);
 void list_set_item(list_t *list, size_t index, void *item);
--- src/listmodel.h
+++ src/listmodel.h
@@ -28,7 +28,7 @@ size_t listmodel_count(struct listmodel *model);
 size_t listmodel_render(struct listmodel *model, wchar_t *buffer, size_t len, size_t width, size_t index);
 void listmodel_setmark(struct listmodel *model, size_t index, bool mark);
 bool listmodel_ismarked(struct listmodel *model, size_t index);
-bool listmodel_register_change_callback(struct listmodel *model, model_change_callback callback, void *data);
+bool listmodel_register_change_callback(struct listmodel *model, model_change_callback callback, void *data) __attribute__((warn_unused_result));
 void listmodel_unregister_change_callback(struct listmodel *model, model_change_callback callback, void *data);
 
 #endif
