056d243d9b44372921aa3de521f606162f87938c
Christian Glindkamp <christian.glindkamp@gmail.com>
2018-05-26 07:32:07 +0200

    filedata: ignore file sizes for directories when comparing
    Additionally, the comparision of filenames was inverted for the
    descending file size comparision. This is now also fixed.

--- src/filedata.c
+++ src/filedata.c
@@ -46,14 +46,15 @@ int filedata_listcompare_directory_size_filename(const void *a, const void *b)
 	struct filedata *filedata1 = *(struct filedata **)a;
 	struct filedata *filedata2 = *(struct filedata **)b;
 
-	if((!S_ISDIR(filedata1->stat.st_mode) && !S_ISDIR(filedata2->stat.st_mode)) ||
-	   ( S_ISDIR(filedata1->stat.st_mode) &&  S_ISDIR(filedata2->stat.st_mode))) {
+	if(!S_ISDIR(filedata1->stat.st_mode) && !S_ISDIR(filedata2->stat.st_mode)) {
 		if(filedata1->stat.st_size < filedata2->stat.st_size)
 			return -1;
 		else if(filedata1->stat.st_size > filedata2->stat.st_size)
 			return 1;
 		return strcoll(filedata1->filename, filedata2->filename);
 	}
+	if(S_ISDIR(filedata1->stat.st_mode) && S_ISDIR(filedata2->stat.st_mode))
+		return strcoll(filedata1->filename, filedata2->filename);
 	if(S_ISDIR(filedata1->stat.st_mode))
 		return -1;
 	return 1;
@@ -64,14 +65,15 @@ int filedata_listcompare_directory_size_filename_descending(const void *a, const
 	struct filedata *filedata1 = *(struct filedata **)a;
 	struct filedata *filedata2 = *(struct filedata **)b;
 
-	if((!S_ISDIR(filedata1->stat.st_mode) && !S_ISDIR(filedata2->stat.st_mode)) ||
-	   ( S_ISDIR(filedata1->stat.st_mode) &&  S_ISDIR(filedata2->stat.st_mode))) {
+	if(!S_ISDIR(filedata1->stat.st_mode) && !S_ISDIR(filedata2->stat.st_mode)) {
 		if(filedata1->stat.st_size < filedata2->stat.st_size)
 			return 1;
 		else if(filedata1->stat.st_size > filedata2->stat.st_size)
 			return -1;
-		return -strcoll(filedata1->filename, filedata2->filename);
+		return strcoll(filedata1->filename, filedata2->filename);
 	}
+	if(S_ISDIR(filedata1->stat.st_mode) && S_ISDIR(filedata2->stat.st_mode))
+		return strcoll(filedata1->filename, filedata2->filename);
 	if(S_ISDIR(filedata1->stat.st_mode))
 		return -1;
 	return 1;
