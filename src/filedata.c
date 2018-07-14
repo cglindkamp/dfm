7401e72ca8a0951f949fdc331c3f377060d3000e
Christian Glindkamp <christian.glindkamp@gmail.com>
2018-07-14 15:20:40 +0200

    dirmodel: move filedata output formatting functions to filedata

--- src/filedata.c
+++ src/filedata.c
@@ -10,6 +10,16 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include <wchar.h>
+
+#define INFO_SEPARATOR        L" "
+#define INFO_SEPARATOR_LENGTH 1
+#define INFO_LINK             L"-> "
+#define INFO_LINK_BROKEN      L"-X "
+#define INFO_LINK_LENGTH      3
+#define INFO_DIR              L"<DIR>"
+#define INFO_SIZE_OVERFLOW    L">9000"
+#define INFO_SIZE_DIR_LENGTH  5
 
 int filedata_listcompare_filename(const void *a, const void *b)
 {
@@ -179,6 +189,117 @@ void filedata_format_output(const struct filedata *filedata, char *buffer)
 	length += strftime(buffer + length, sizeof("1970-01-01 00:00:00"), "%F %T", &modification_time);
 
 	buffer[length] = 0;
+}
+
+static void filesize_to_string(wchar_t *buf, off_t filesize)
+{
+	char suffix[] = {' ', 'K', 'M', 'G', 'T', 'P', 'E'};
+	size_t cs = 0;
+	off_t cv = filesize;
+
+	while(cv > 1024 && cs < sizeof(suffix) - 1) {
+		filesize = cv;
+		cv /= 1024;
+		cs++;
+	}
+
+	if(cv >= 10000)
+		wcscpy(buf, INFO_SIZE_OVERFLOW);
+	else if(cv < 100 && cs > 0)
+		swprintf(buf, 6, L"%2lu.%1lu%c", cv, (filesize - cv * 1024) * 10 / 1024, suffix[cs]);
+	else
+		swprintf(buf, 6, L"%4lu%c", cv, suffix[cs]);
+}
+
+static size_t render_info(wchar_t *buffer, struct filedata *filedata)
+{
+	size_t info_size = INFO_SIZE_DIR_LENGTH + 1;
+
+	wcscpy(buffer, INFO_SEPARATOR);
+
+	if(filedata->is_link) {
+		info_size += INFO_LINK_LENGTH;
+		if(filedata->is_link_broken)
+			wcscat(buffer, INFO_LINK_BROKEN);
+		else
+			wcscat(buffer, INFO_LINK);
+	}
+
+	if(S_ISDIR(filedata->stat.st_mode))
+		wcscat(buffer, INFO_DIR);
+	else
+		filesize_to_string(buffer + wcslen(buffer), filedata->stat.st_size);
+
+	return info_size;
+}
+
+static size_t render_filename(wchar_t *buffer, size_t len, size_t width, const char *filename)
+{
+	size_t wc_count = mbstowcs(NULL, filename, 0);
+
+	if(wc_count == (size_t)-1) {
+		buffer[0] = L'\0';
+		if(len > 3)
+			len = 3;
+		wcsncat(buffer, L"???", len);
+		return wcslen(buffer);
+	}
+
+	wchar_t name[wc_count + 1];
+
+	mbstowcs(name, filename, wc_count);
+
+	size_t char_count = 0, display_count = 0;
+
+	for(size_t i = 0; i < wc_count; i++) {
+		int w = wcwidth(name[i]);
+		if(w == -1)
+			continue;
+		if(w + display_count > width)
+			break;
+		if(char_count < len)
+			buffer[char_count] = name[i];
+		display_count += w;
+		char_count++;
+	}
+	buffer[char_count] = L'\0';
+
+	return char_count;
+}
+
+size_t filedata_format_list_line(struct filedata *filedata, wchar_t *buffer, size_t len, size_t width)
+{
+	wchar_t info[INFO_SEPARATOR_LENGTH + INFO_LINK_LENGTH + INFO_SIZE_DIR_LENGTH + 1];
+	size_t info_size;
+	size_t char_count = 0;
+	size_t display_count = 0;
+
+	info_size = render_info(info, filedata);
+
+	if(width > info_size) {
+		char_count = render_filename(buffer, len - info_size, width - info_size, filedata->filename);
+		if(char_count + info_size > len)
+			return char_count + info_size;
+
+		display_count = wcswidth(buffer, len);
+	} else
+		info_size = width;
+
+
+	if(display_count + info_size < width) {
+		size_t padlength = width - display_count - info_size;
+
+		if(char_count + padlength + info_size <= len)
+			wmemset(&buffer[char_count], L' ', padlength);
+
+		char_count += padlength;
+	}
+	buffer[char_count] = L'\0';
+
+	if(char_count + info_size <= len)
+		wcsncat(buffer, info + wcslen(info) - info_size, info_size);
+
+	return char_count + info_size;
 }
 
 int filedata_new_from_file(struct filedata **filedata, int dirfd, const char *filename)
--- src/filedata.h
+++ src/filedata.h
@@ -24,6 +24,7 @@ int filedata_listcompare_directory_mtime_filename_descending(const void *a, cons
 
 #define FILEDATA_FORMAT_OUTPUT_BUFFER_SIZE (sizeof("drwxrwxrwx 1970-01-01 00:00:00") + 2 * 33)
 void filedata_format_output(const struct filedata *filedata, char *buffer);
+size_t filedata_format_list_line(struct filedata *filedata, wchar_t *buffer, size_t len, size_t width);
 
 int filedata_new_from_file(struct filedata **filedata, int dirfd, const char *filename);
 void filedata_delete(struct filedata *filedata);
